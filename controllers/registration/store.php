22cd79b1fd0740fe29fb2ca3334330c105bfcd81
Jose Castillo <jose@blinkist.com>
2024-02-11 19:06:41 +0100

    encrypt passwords

--- controllers/registration/store.php
+++ controllers/registration/store.php
@@ -31,7 +31,7 @@ if ($user) {
     $db->query(
         "insert into users(email, password) values(:email, :password)", [
             "email" => $email,
-            "password" => $password
+            "password" => password_hash($password, PASSWORD_DEFAULT)
         ]
     );
 
